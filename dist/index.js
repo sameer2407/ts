c95a060d632acb0cf22b83954d4774158f007810
sameer2407 <satyamjadaun2001@gmail.com>
2024-09-14 09:54:33 +0530

    first

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let age = 30;
+if (age < 50) {
+    age += 10;
+    console.log(age);
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let age: number=30;
+if(age<50)
+{
+  age += 10;
+  console.log(age);
+}

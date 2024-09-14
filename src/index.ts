6d9c4ed183cda4e1e1871586906c527caf6411d6
sameer2407 <satyamjadaun2001@gmail.com>
2024-09-14 10:59:31 +0530

    obj

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,30 @@
 let arr:number[]=[];
  arr[0]=45;
 console.log(typeof(arr));
+
+function calculateAge(birthYear:number):number
+{
+  return 2024-birthYear;
+}
+function caclculatetax(income:number):number{
+  if(income<5000)
+  {
+    return income*0.1;
+  }
+  else
+  {
+    return income*0.2;
+  }
+}
+
+function  incomeTaxByYear(salary:number,year?:number):number{
+        if((year||2022)>2034){
+          return salary*0.2;
+        }
+        return salary*0.1;
+}
+
+let employee:{ readonly name:string,age:number}={
+  name:"John",
+  age:30
+}
